ed8393cbac8fcf1b6b0c7352eb10c291ff197883
luisjcl <l2435073560101@gmail.com>
2024-09-04 00:06:45 -0600

    Inicio Store

---
+++ src/app/[[...product]]/page.tsx
@@ -0,0 +1,39 @@
+import type {Metadata} from "next";
+
+import api from "~/product/api";
+
+import StoreScreen from "@/modules/store/screens/Store";
+
+export async function generateStaticParams() {
+  const products = await api.list();
+
+  // Build index for the home page and then for each product
+  return [{}].concat(products.map((product) => ({product: [product.id]}))) as Record<
+    string,
+    string[]
+  >[];
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: {product?: string[]};
+}): Promise<Metadata | undefined> {
+  if (params.product) {
+    const product = await api.fetch(params.product[0]);
+
+    return {
+      title: product.title,
+      description: product.description,
+    };
+  }
+}
+
+async function HomeAndProductPage({params}: {params: {product?: [product: string]}}) {
+  const products = await api.list();
+  const selected = params.product ? await api.fetch(params.product[0]) : null;
+
+  return <StoreScreen products={products} selected={selected} />;
+}
+
+export default HomeAndProductPage;
